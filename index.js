b221d6de853df5c87d4abfaabda4607f8b4150ab
LujanSolo <lujansolo@proton.me>
2023-05-06 00:12:47 -0700

    added initial js button functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+let userInput = document.getElementById("input-field");
+let addButton = document.getElementById("add-btn");
+
+addButton.addEventListener("click", () => {
+  console.log(userInput);
+})
