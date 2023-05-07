2f2f3606668f23ab713c2a46c576925173ca54d3
LujanSolo <lujansolo@proton.me>
2023-05-07 14:43:13 -0700

    added notes to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
   getDatabase,
@@ -5,16 +6,20 @@ import {
   push,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+// database setup
 const appSettings = {
   databaseURL: "https://playground-a18ce-default-rtdb.firebaseio.com/",
 };
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const infoInDB = ref(database, "info");
+
+// pull in elements from html
 const userInputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-btn");
 let listField = document.getElementById("shopping-list");
 
+// event listener on "Add Item" click
 addButtonEl.addEventListener("click", () => {
   let inputValue = userInputEl.value;
   push(infoInDB, inputValue);
@@ -23,9 +28,12 @@ addButtonEl.addEventListener("click", () => {
   clearInputField();
 });
 
+// function to add list item to ul
 function createListField(inherentValue) {
   listField.innerHTML += `<li>${inherentValue}</li>`;
-};
+}
+
+// function to clear Input Field upon button click
 function clearInputField() {
   userInputEl.value = "";
-};
+}
