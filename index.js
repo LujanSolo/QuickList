075ede607842551bd5cb7406ce0ad170ca6b6ca0
LujanSolo <lujansolo@proton.me>
2023-05-07 11:50:39 -0700

    imported ref from firebase and added new const to hold database ref

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://playground-a18ce-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings);
-console.log(app)
+const database = getDatabase(app);
+const infoInDB = ref(database, "info");
 
 const userInputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-btn");
