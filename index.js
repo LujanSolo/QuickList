9f2cac641e552fb6cc41d8bfd4eeefea21840442
LujanSolo <lujansolo@proton.me>
2023-05-07 14:21:19 -0700

    refactored to include methods in eventListener

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://playground-a18ce-default-rtdb.firebaseio.com/"
-}
-
+  databaseURL: "https://playground-a18ce-default-rtdb.firebaseio.com/",
+};
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const infoInDB = ref(database, "info");
-
 const userInputEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-btn");
 let listField = document.getElementById("shopping-list");
+
+
 
 addButtonEl.addEventListener("click", () => {
   let inputValue = userInputEl.value;
   push(infoInDB, inputValue);
   console.log(`${inputValue} added to the database`);
-  listField.innerHTML += `<li>${inputValue}</li>`;
-  userInputEl.value = "";
+  createListField();
+  clearField();
+
+  function createListField() {
+    listField.innerHTML += `<li>${inputValue}</li>`;
+  };
+  function clearField() {
+    userInputEl.value = "";
+  };
 });
+
