96cf2a5bfd50bd2ac47373ef69bb0e20a6b2a2af
LujanSolo <lujansolo@proton.me>
2023-05-08 22:14:41 -0700

    refactored code to remove duplications to list items; all updates will be pushed to firebase first, then rendered to user's list

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const shopListinDB = ref(database, "info");
-// const booksInDB = ref(database, "books");
 
 // pull in elements from html
 const userInputEl = document.getElementById("input-field");
@@ -23,29 +22,27 @@ let listField = document.getElementById("shopping-list");
 
 onValue(shopListinDB, function (snapshot) {
   let shopListArray = Object.values(snapshot.val());
+  clearListField();
   for (let i=0; i < shopListArray.length; i++){
     createListField(shopListArray[i]);
   }
   console.log(shopListArray)
 })
 
-// onValue(booksInDB, function (snapshot) {
-//   let booksArray = Object.values(snapshot.val());
-//   console.log(booksArray)
-// })
-
 // event listener on "Add Item" click
 addButtonEl.addEventListener("click", () => {
   let inputValue = userInputEl.value;
-  push(infoInDB, inputValue);
-  console.log(`${inputValue} added to the database`);
-  createListField(inputValue);
+  push(shopListinDB, inputValue);
   clearInputField();
 });
 
 // function to add list item to ul
 function createListField(inherentValue) {
   listField.innerHTML += `<li>${inherentValue}</li>`;
+}
+
+function clearListField() {
+  listField.innerHTML = "";
 }
 
 // function to clear Input Field upon button click
